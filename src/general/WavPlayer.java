c01e10f1ecd1e58c21c88c38dbf5c25e8ec85b19
Benjamin Lempereur <contact@benjaminlempereur.com>
2016-05-10 16:49:02 +0200

    WavPlayer class

---
+++ src/general/WavPlayer.java
@@ -0,0 +1,74 @@
+package general;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * <b>Wav layer is the class for reading and controlling a wav file.</b>
+ *
+ * @author Lempereur Benjamin
+ *
+ * @version 1.0
+ */
+final public class WavPlayer {
+    /**
+     * The sound in memory.
+     *
+     * @see WavPlayer#play(String)
+     */
+    private static Clip _clip = null;
+
+    /**
+     * Method to start the wav file whose path is passed as parameter.
+     * @param filepath
+     *       The path of the wav file.
+     */
+    public static void play(String filepath) {
+        try {
+            _clip = AudioSystem.getClip();
+            File file = new File(filepath);
+            if(file.exists()) {
+                AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+                _clip.open(ais);
+            }
+            else
+                throw new RuntimeException("WavPlayer: file not found: " + filepath);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            throw new RuntimeException("WavPlayer: Line Unavailable Exception Error: " + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("WavPlayer: Input/Output Error: " + e);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+            throw new RuntimeException("WavPlayer: Unsupported Audio File: " + e);
+        }
+
+    }
+
+    /**
+     * Method to read the file continuously.
+     *
+     */
+    public static void loop() {
+        _clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    /**
+     * Method to stop the reading.
+     *
+     */
+    public static void stop() {
+        _clip.stop();
+    }
+
+    /**
+     * Method to replay the wav file.
+     *
+     */
+    public static void replay() {
+        _clip.setFramePosition(0);
+        _clip.start();
+    }
+}
