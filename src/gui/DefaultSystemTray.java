c700b1ebf77305695f8e044a78f866abfa4b3b0e
Benjamin Lempereur <contact@benjaminlempereur.com>
2016-05-11 14:15:04 +0200

    Date class, Logfile class, SystemTray class

--- src/gui/DefaultSystemTray.java
+++ src/gui/DefaultSystemTray.java
@@ -5,11 +5,37 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Objects;
 
-//FAIRE JAVADOC
+/**
+ * <b>LogFileBean is a class used to manage a System Tray.</b>
+ *
+ * @author Lempereur Rocca
+ *
+ *
+ * @version 1.0
+ */
 public class DefaultSystemTray {
+    /**
+     * The menu of the System Tray.
+     *
+     * @see DefaultSystemTray#DefaultSystemTray()
+     * @see DefaultSystemTray#DefaultSystemTray(Image, String)
+     */
     private PopupMenu _trayMenu;
+
+    /**
+     * The list of the items in the menu.
+     *
+     * @see DefaultSystemTray#DefaultSystemTray()
+     * @see DefaultSystemTray#DefaultSystemTray(Image, String)
+     */
     private ArrayList<MenuItem> _menuList;
 
+    /**
+     * DefaultSystemTray default Constructor.
+     * <p>
+     *     Instantiation of the menu and the list of items.
+     * </p>
+     */
     public DefaultSystemTray() {
         if(SystemTray.isSupported()) {
             _trayMenu = new PopupMenu();
@@ -19,6 +45,17 @@ public class DefaultSystemTray {
             throw new RuntimeException("SystemTray: SystemTray is not supported");
     }
 
+    /**
+     * DefaultSystemTray Constructor.
+     * <p>
+     *     Instantiation of the menu and the list of items and adding the new tray to the System Tray.
+     * </p>
+     *
+     * @param icone
+     *          Icon of the Tray.
+     * @param caption
+     *          Text of the Tray.
+     */
     public DefaultSystemTray(Image icone, String caption) {
         if(SystemTray.isSupported()) {
             _trayMenu = new PopupMenu();
@@ -38,6 +75,17 @@ public class DefaultSystemTray {
             throw new RuntimeException("SystemTray: SystemTray is not supported");
     }
 
+    /**
+     * Method to create the Tray.
+     * <p>
+     *     Use to add the new tray to the System Tray after using the default constructor.
+     * </p>
+     *
+     * @param icone
+     *          Icon of the Tray.
+     * @param caption
+     *          Text of the Tray.
+     */
     public void create(Image icone, String caption) {
         TrayIcon trayIcon = new TrayIcon(icone, caption, _trayMenu);
         trayIcon.setImageAutoSize(true);
@@ -51,14 +99,39 @@ public class DefaultSystemTray {
         }
     }
 
+    /**
+     * Method to add an option to the tray.
+     * <p>
+     *     Use to add an ption to the tray.
+     * </p>
+     *
+     * @param name
+     *          Name of the option.
+     * @param action
+     *          Action of the option.
+     */
     public void add(String name, ActionListener action) {
+        for (MenuItem item:_menuList) {
+            if(Objects.equals(name, item.getLabel())) {
+                throw new RuntimeException("SystemTray: Option already added");
+            }
+        }
         MenuItem optionTrayMenu = new MenuItem(name);
         optionTrayMenu.addActionListener(action);
         _trayMenu.add(optionTrayMenu);
         _menuList.add(optionTrayMenu);
     }
 
-    public void delete(String name) {
+    /**
+     * Method to remove an option of the tray.
+     * <p>
+     *     Use to remove an option of the tray.
+     * </p>
+     *
+     * @param name
+     *          Name of the option.
+     */
+    public void remove(String name) {
         _menuList.stream().filter(item -> Objects.equals(item.getLabel(), name)).forEach(item -> _trayMenu.remove(item));
     }
 }
