9c7eba7a7e3a8d6b36567ddaebb316002e619aa3
Benjamin Lempereur <contact@benjaminlempereur.com>
2016-05-11 14:38:32 +0200

    Date class, Logfile class, SystemTray class

--- src/date/FormattedDate.java
+++ src/date/FormattedDate.java
@@ -1,68 +1,178 @@
 package date;
 
 import java.text.DateFormat;
-import java.util.Calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-//FAIRE JAVADOC + Verifdate
-
+/**
+ * <b>FormattedDate is a class used to get formatted dates.</b>
+ *
+ * @author Lempereur Rocca
+ *
+ * @version 1.0
+ */
 public class FormattedDate {
+    /**
+     * The date format.
+     *
+     * @see FormattedDate#FormattedDate()
+     * @see FormattedDate#FormattedDate(int, int, Locale)
+     */
     private int formatDate;
-    private int formatHeure;
-    private Locale localeDate;
 
+    /**
+     * The time format.
+     *
+     * @see FormattedDate#FormattedDate()
+     * @see FormattedDate#FormattedDate(int, int, Locale)
+     */
+    private int formatTime;
+
+    /**
+     * The Locale.
+     *
+     * @see FormattedDate#FormattedDate()
+     * @see FormattedDate#FormattedDate(int, int, Locale)
+     */
+    private Locale locale;
+
+    /**
+     * FormattedDate default Constructor.
+     * <p>
+     *     Formats are set to FULL and the locale is set to the default Locale.
+     * </p>
+     */
     public FormattedDate() {
         formatDate = DateFormat.FULL;
-        formatHeure = DateFormat.FULL;
-        localeDate = Locale.getDefault();
+        formatTime = DateFormat.FULL;
+        locale = Locale.getDefault();
     }
 
-    public FormattedDate(int formatDate, int formatHeure, Locale localeDate) {
+    /**
+     * FormattedDate Constructor.
+     *
+     * @param formatDate
+     *          The date format.
+     * @param formatTime
+     *          The time format.
+     * @param locale
+     *          The Locale.
+     */
+    public FormattedDate(int formatDate, int formatTime, Locale locale) {
         this.formatDate = formatDate;
-        this.formatHeure = formatHeure;
-        this.localeDate = localeDate;
+        this.formatTime = formatTime;
+        this.locale = locale;
     }
 
+    /**
+     * Return the formatted datetime.
+     *
+     * @return The formatted datetime.
+     */
+    public String getFormattedDateTime() {
+        return DateFormat.getDateTimeInstance(formatDate, formatTime, locale).format(new Date());
+    }
+
+    /**
+     * Return the formatted date.
+     *
+     * @return The formatted date.
+     */
     public String getFormattedDate() {
-        return DateFormat.getDateTimeInstance(formatDate, formatHeure, localeDate).format(new Date());
+        return DateFormat.getDateInstance(formatDate, locale).format(new Date());
     }
 
+    /**
+     * Return the formatted time.
+     *
+     * @return The formatted time.
+     */
+    public String getFormattedTime() {
+        return DateFormat.getTimeInstance(formatTime, locale).format(new Date());
+    }
+
+    /**
+     * Return the format of the date.
+     *
+     * @return The format of the date.
+     */
     public int getFormatDate() {
         return formatDate;
     }
 
+    /**
+     * Updates the format of the date.
+     *
+     * @param formatDate
+     *          The new format of the date.
+     */
     public void setFormatDate(int formatDate) {
         this.formatDate = formatDate;
     }
 
-    public int getFormatHeure() {
-        return formatHeure;
+    /**
+     * Return the format of the time.
+     *
+     * @return The format of the time.
+     */
+    public int getFormatTime() {
+        return formatTime;
     }
 
-    public void setFormatHeure(int formatHeure) {
-        this.formatHeure = formatHeure;
+    /**
+     * Updates the format of the time.
+     *
+     * @param formatTime
+     *          The new format of the time.
+     */
+    public void setFormatTime(int formatTime) {
+        this.formatTime = formatTime;
     }
 
-    public Locale getLocaleDate() {
-        return localeDate;
+    /**
+     * Return the locale of the datetime.
+     *
+     * @return The lcoale of the datetime.
+     */
+    public Locale getLocale() {
+        return locale;
     }
 
-    public void setLocaleDate(Locale localeDate) {
-        this.localeDate = localeDate;
+    /**
+     * Updates the locale of the datetime.
+     *
+     * @param locale
+     *          The new locale of the datetime.
+     */
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 
-    public static boolean dateValidator(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setLenient(false);
-        cal.setTime(date);
+    /**
+     * Method used to check if a date is valid with a format.
+     *
+     * @param dateToValidate
+     *       The date to validate.
+     * @param format
+     *       The format.
+     *
+     * @return The result of the validation.
+     */
+    public static boolean dateValidator(String dateToValidate, String format) {
+        if(dateToValidate == null)
+            return false;
+
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        sdf.setLenient(false);
+
         try {
-            cal.getTime();
-            return true;
+            sdf.parse(dateToValidate);
+        } catch (ParseException e) {
+            throw new RuntimeException("FormattedDate: Date is not a valid date : " + e);
         }
-        catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("FormattedDate: Date is not a valid date");
-        }
+
+        return true;
     }
 }
